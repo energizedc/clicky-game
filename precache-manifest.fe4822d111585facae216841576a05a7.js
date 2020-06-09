be2188dffae897ac316134a488690f278c9c3065
Donna Johnson <tjndlc@comcast.net>
2020-06-08 17:46:21 -0700

    Updates

---
+++ precache-manifest.fe4822d111585facae216841576a05a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67abce3318ce9309e3ecbfb8800fb4b6",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "3c286e12f66a35b16509",
+    "url": "/clicky-game/static/css/main.3b49633e.chunk.css"
+  },
+  {
+    "revision": "55ea0a1c41e3cf21f12c",
+    "url": "/clicky-game/static/js/2.378b37ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.378b37ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3c286e12f66a35b16509",
+    "url": "/clicky-game/static/js/main.1d76646e.chunk.js"
+  },
+  {
+    "revision": "ddea091f236f02df6e9c",
+    "url": "/clicky-game/static/js/runtime-main.de0ba425.js"
+  }
+]);
